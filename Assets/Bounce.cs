2f60a3f5ec7440d157f8f55cb5d75c8273668de9
SelinaDaley <sdmd6@hotmail.com>
2017-02-12 21:19:05 -0500

    initial commit

---
+++ Assets/Bounce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+    private LineRenderer line;
+    public Transform origin;
+    public Transform destin;
+
+    // Use this for initialization
+    void Start () {
+        line = GetComponent<LineRenderer>();
+        line.startWidth = 0.1f;
+        line.endWidth = 0.1f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        line.SetPosition(0, origin.position);
+        line.SetPosition(1, destin.position);
+    }
+
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject weight;
+    public Text weightText;
+    public Text minText;
+    public Text maxText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeMinText(string value)
+    {        
+        weight.GetComponent<SpringJoint>().minDistance = float.Parse(value);
+    }
+
+    public void ChangeMaxText(string value)
+    {
+        weight.GetComponent<SpringJoint>().maxDistance = float.Parse(value);
+    }
+
+    public void ChangeMass(float value)
+    {
+        weightText.text = "Weight: " + value;
+        weight.GetComponent<Rigidbody>().mass = value;
+    }
+}
